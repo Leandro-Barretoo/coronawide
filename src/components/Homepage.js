85351cd9d56b3b916dcfd6a2b12c64cefe49583f
Leandro <officialdevmi@gmail.com>
2021-09-13 11:40:47 -0700

    Add images and styles to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,20 @@
 import './Homepage.css';
 import Continent from './Continent';
+import africa from '../assets/africa.png';
+import europe from '../assets/europe.png';
+import asia from '../assets/asia.png';
+import northAmerica from '../assets/americanorte.png';
+import southAmerica from '../assets/americasul.png';
+import australia from '../assets/australia.png';
 
 const Homepage = () => (
   <div className="Continents-Container">
-    <Continent />
-    <Continent />
-    <Continent />
-    <Continent />
+    <Continent image={africa} name="AFRICA" />
+    <Continent image={europe} name="EUROPE" />
+    <Continent image={asia} name="ASIA" />
+    <Continent image={australia} name="AUSTRALIA" />
+    <Continent image={northAmerica} name="NORTH AMERICA" />
+    <Continent image={southAmerica} name="SOUTH AMERICA" />
   </div>
 );
 
