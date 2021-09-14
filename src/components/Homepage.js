d55a365fd27651dcd371ef33bc2e5e3daa643c11
Leandro <officialdevmi@gmail.com>
2021-09-14 03:53:33 -0700

    fetch countries from api

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,13 +7,44 @@ import europe from '../assets/europeblue.png';
 import asia from '../assets/asiablue.png';
 import northAmerica from '../assets/northamericablue.png';
 import southAmerica from '../assets/southamericablue.png';
+import europeArr from '../countriesArrays/europeArr';
+import africaArr from '../countriesArrays/africaArr';
+import asiaArr from '../countriesArrays/asiaArr';
+import northAmericaArr from '../countriesArrays/northAmericaArr';
+import southAmericaArr from '../countriesArrays/southAmericaArr';
+import { addEurope } from '../redux/covid/europe';
+import { addAfrica } from '../redux/covid/africa';
+import { addAsia } from '../redux/covid/asia';
+import { addNorthAmerica } from '../redux/covid/northAmerica';
+import { addSouthAmerica } from '../redux/covid/southAmerica';
 
 const Homepage = () => {
   const dispatch = useDispatch();
+  const africaCountries = useSelector((state) => state.africaReducer);
 
   const getCountries = () => (dispatch) => {
-    fetch('')
-  }
+    fetch('https://api.covid19tracking.narrativa.com/api/countries')
+      .then((response) => response.json())
+      .then((data) => data.countries.forEach((country) => {
+        if (europeArr.includes(country.id)) {
+          dispatch(addEurope(country));
+        } else if (africaArr.includes(country.id)) {
+          dispatch(addAfrica(country));
+        } else if (asiaArr.includes(country.id)) {
+          dispatch(addAsia(country));
+        } else if (northAmericaArr.includes(country.id)) {
+          dispatch(addNorthAmerica(country));
+        } else if (southAmericaArr.includes(country.id)) {
+          dispatch(addSouthAmerica(country));
+        }
+      }));
+  };
+
+  useEffect(() => {
+    if (!africaCountries.length) {
+      dispatch(getCountries());
+    }
+  }, []);
 
   return (
     <div className="Continents-Container">
